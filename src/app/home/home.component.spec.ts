0260e1cc1d204960364ef8eb116eb76f5a9e83a4
motapod <isaac.riveros.ex@copec.cl>
2024-07-18 19:49:43 -0400

    Update home.component.spec.ts

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { CurrencyService } from './currency.service';
+import { CurrencyService } from '../currency.service';
 
 describe('CurrencyService', () => {
   let service: CurrencyService;
