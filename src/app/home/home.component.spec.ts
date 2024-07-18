b1607cb4c96e9d450270a8e15f82df6918c221ac
motapod <isaac.riveros.ex@copec.cl>
2024-07-18 19:48:03 -0400

    Update home.component.spec.ts

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,23 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CurrencyService } from './currency.service';
 
-import { HomeComponent } from './home.component';
+describe('CurrencyService', () => {
+  let service: CurrencyService;
+  let httpMock: HttpTestingController;
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CurrencyService]
+    });
+    service = TestBed.inject(CurrencyService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get exchange rate', () => {
+    const mockResponse = { result: 'success', data: { rate: 1.2 } };
+    const baseCurrency = 'USD';
+    const targetCurrency = 'EUR';
+
+    service.getExchangeRate(baseCurrency, targetCurrency).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}${service['apiKey']}/pair/${baseCurrency}/${targetCurrency}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should store conversion', () => {
+    const mockResponse = { result: 'success' };
+    const data = { baseCurrency: 'USD', targetCurrency: 'EUR', rate: 1.2 };
+
+    service.storeConversion(data).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(service['lambdaStoreUrl']);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(data);
+    req.flush(mockResponse);
+  });
+
+  it('should get conversions', () => {
+    const mockResponse = [{ baseCurrency: 'USD', targetCurrency: 'EUR', rate: 1.2 }];
+    const currency = 'USD';
+
+    service.getConversions(currency).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${service['lambdaGetUrl']}/${currency}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
   });
 });
